8273c6cbc203ed6af39a0d9d3a72ed8a7a0e5748
Marc <magueracoras@gmail.com>
2019-10-24 17:07:45 +0200

    Main Menu

--- Magical Mystery Shop/Assets/Scripts/LoadingScreen.cs
+++ Magical Mystery Shop/Assets/Scripts/LoadingScreen.cs
@@ -9,6 +9,25 @@ public class LoadingScreen : MonoBehaviour
 {
     [SerializeField] private Image m_loadingImage;
 
+    public void FadeOut(float _time, Action _onLoaded = null)
+    {
+        m_loadingImage.gameObject.SetActive(true);
+        m_loadingImage.DOFade(1, 0).OnComplete(() => 
+        {
+            if (_onLoaded != null)_onLoaded.Invoke();
+        });
+        m_loadingImage.DOFade(0, _time).OnComplete(() =>
+        {
+            m_loadingImage.gameObject.SetActive(false);
+        });
+    }
+
+    public void FadeIn(float _time, Action _onLoaded = null)
+    {
+        m_loadingImage.gameObject.SetActive(true);
+        m_loadingImage.DOFade(1, _time).OnComplete(_onLoaded.Invoke);
+    }
+
     public void FadeOutIn(float _time, Action _onLoaded = null)
     {
         m_loadingImage.gameObject.SetActive(true);
---
+++ Magical Mystery Shop/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] private LoadingScreen loadingScreen;
+    [SerializeField] private string gameSceneName;
+    [SerializeField] private GameEvent optionsEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadingScreen.FadeOut(0.5f);
+    }
+
+    public void PlayButton()
+    {
+        loadingScreen.FadeIn(0.5f, LoadGameScene);
+    }
+
+    public void OpenOptionsWindow()
+    {
+        optionsEvent.Raise();
+    }
+
+    private void LoadGameScene()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
