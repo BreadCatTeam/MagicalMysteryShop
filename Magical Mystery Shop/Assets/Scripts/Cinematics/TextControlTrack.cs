6f7e1930f7462214266b5fa5194e18328c8bfb51
Marc <magueracoras@gmail.com>
2019-10-28 19:30:55 +0100

    Cinematics: the Begining

---
+++ Magical Mystery Shop/Assets/Scripts/Cinematics/TextControlTrack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using TMPro;
+
+[TrackColor(86f/255f, 191f/255f, 1)]
+[TrackBindingType(typeof(TextMeshProUGUI))]
+[TrackClipType(typeof(TextControlClip))]
+public class TextControlTrack : TrackAsset
+{
+    
+}
