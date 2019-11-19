6603b45f7056e17a32b0996e11c6629018e42255
Marc <magueracoras@gmail.com>
2019-11-19 15:50:47 +0100

    Hola soy el Marc jaja si

---
+++ Magical Mystery Shop/Assets/Scripts/Clients/ClientMaterial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientMaterial : MonoBehaviour
+{
+    [SerializeField] Renderer m_renderer;
+    [SerializeField] Material[] m_bodyMaterial;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        m_renderer.material = m_bodyMaterial[Random.Range(0, m_bodyMaterial.Length)];
+    }
+}
