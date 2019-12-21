9cfc293cebab8a1980f1a5d2989528d62a3409c6
Marc <magueracoras@gmail.com>
2019-12-21 21:21:42 +0100

    Set Camera to Scene view tool

---
+++ Magical Mystery Shop/Assets/Scripts/Editor/SceneCameraView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SceneCameraView: EditorWindow
+{
+    [MenuItem("Window/SceneCameraView")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        SceneCameraView window = (SceneCameraView)EditorWindow.GetWindow(typeof(SceneCameraView));
+    }
+
+    void OnGUI()
+    {
+        if(GUILayout.Button("Set Camera to View"))
+        {
+            //Camera.main.transform.position = SceneView.lastActiveSceneView.position.position;
+            Camera.main.transform.position = SceneView.lastActiveSceneView.camera.transform.position;
+            Camera.main.transform.rotation = SceneView.lastActiveSceneView.camera.transform.rotation;
+        }
+    }
+}
