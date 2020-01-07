e0c0cd2c331f72bf97780be8ab91127033fb6f2e
Marc <magueracoras@gmail.com>
2020-01-07 16:23:50 +0100

    Revert "Revert "Candles' Flame FX""
    This reverts commit 89a79639a70cfc16a012d20bc217cb67332c13a2.

--- Magical Mystery Shop/Assets/Scripts/UI/Billboard.cs
+++ Magical Mystery Shop/Assets/Scripts/UI/Billboard.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 public class Billboard : MonoBehaviour
 {
     private Camera m_Camera;
-    private Canvas canvas;
+    //private Canvas canvas;
 
     private void Start()
     {
         m_Camera = Camera.main;
-        canvas = GetComponent<Canvas>();
-        canvas.worldCamera = m_Camera;
+        //canvas = GetComponent<Canvas>();
+        //canvas.worldCamera = m_Camera;
     }
 
     //Orient the camera after all movement is completed this frame to avoid jittering
