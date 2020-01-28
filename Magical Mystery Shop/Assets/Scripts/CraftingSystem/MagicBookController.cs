ac77f883eeda7f1777a0051e0d06138414ffbb98
Marc <magueracoras@gmail.com>
2020-01-28 23:41:12 +0100

    Libro magico UI primera iteración

---
+++ Magical Mystery Shop/Assets/Scripts/CraftingSystem/MagicBookController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MagicBookController : MonoBehaviour
+{
+    [SerializeField] ItemDatabase m_itemDatabase;
+    [SerializeField] TextMeshProUGUI m_descriptionBox;
+    [SerializeField] GameObject m_recipeContainer;
+    [SerializeField] Transform m_recipeParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < m_itemDatabase.GetCraftingRecipes().Capacity; i++)
+        {
+            GameObject r = Instantiate(m_recipeContainer, m_recipeParent);
+            RecipeContainer recipe = r.GetComponent<RecipeContainer>();
+            recipe.CraftingRecipe = m_itemDatabase.GetCraftingRecipes()[i];
+            recipe.SetView();
+        }
+    }
+
+
+}
