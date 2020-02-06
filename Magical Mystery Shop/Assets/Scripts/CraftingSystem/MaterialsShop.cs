aa73189b9bbde5920ba7e53f439c63e50dd13df2
Marc <magueracoras@gmail.com>
2020-02-07 00:29:40 +0100

    Tienda de materiales avanazada

--- Magical Mystery Shop/Assets/Scripts/CraftingSystem/MaterialsShop.cs
+++ Magical Mystery Shop/Assets/Scripts/CraftingSystem/MaterialsShop.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MaterialsShop : MonoBehaviour
 {
+    private const int MATERIALS_PER_PAGE = 8;
     [SerializeField] private ItemDatabase itemDatabase;
     [SerializeField] private Transform m_itemsContainer;
     [SerializeField] private GameObject m_materialShopItemGO;
@@ -12,35 +13,83 @@ public class MaterialsShop : MonoBehaviour
     [SerializeField] private int startMaterials = 8;
     [SerializeField] private GameObject m_window;
     [SerializeField] private Transform m_materialsContainer;
+    private int i_currentPage = 0;
+    private int i_materialsToShow;
+    private bool b_opened;
     private MaterialShopItem[] materialShopItem;
+    private int i_maxPages;
+    private int i_materialsPerLevel = 20;
 
     // Start is called before the first frame update
     void Start()
     {
-        LoadBuyingItems();
+        materialShopItem = m_materialsContainer.GetComponentsInChildren<MaterialShopItem>();
+
         CloseWindow();
+        i_maxPages = Mathf.FloorToInt((float)(itemDatabase.Materials.Length - 1) / (float)MATERIALS_PER_PAGE);
     }
 
     void LoadBuyingItems()
     {
-        materialShopItem = m_materialsContainer.GetComponentsInChildren<MaterialShopItem>();
+        ClearItems();
 
-        for (int i = 0; i < materialShopItem.Length; i++)
+        i_materialsToShow = i_currentPage * MATERIALS_PER_PAGE;
+
+        for (int i = i_materialsToShow; i < i_materialsToShow + MATERIALS_PER_PAGE; i++)
         {
             if (i < itemDatabase.Materials.Length)
-                materialShopItem[i].SetView(itemDatabase.Materials[i], m_materialsInventory);
+                materialShopItem[i - i_materialsToShow].SetView(itemDatabase.Materials[i], m_materialsInventory);
             
+        }
+
+        materialShopItem[0].SelectIten();
+    }
+
+    private void ClearItems()
+    {
+        for(int i = 0; i < materialShopItem.Length; i++)
+        {
+            materialShopItem[i].Clear();
         }
     }
 
     public void OpenWindow()
     {
         m_window.SetActive(true);
+        b_opened = true;
+        i_currentPage = 0;
+        LoadBuyingItems();
     }
 
     public void CloseWindow()
     {
         m_window.SetActive(false);
+        b_opened = false;
 
+    }
+
+    public void NextPage()
+    {
+        if (!b_opened)
+            return;
+        i_currentPage++;
+
+        if (i_currentPage > i_maxPages)
+            i_currentPage = 0;
+
+        LoadBuyingItems();
+    }
+
+    public void PrevPage()
+    {
+        if (!b_opened)
+            return;
+
+        i_currentPage--;
+
+        if (i_currentPage < 0)
+            i_currentPage = i_maxPages;
+
+        LoadBuyingItems();
     }
 }
--- Magical Mystery Shop/Assets/Scripts/Item/MaterialShopItem.cs
+++ Magical Mystery Shop/Assets/Scripts/Item/MaterialShopItem.cs
@@ -6,7 +6,7 @@ using TMPro;
 using DG.Tweening;
 using UnityEngine.EventSystems;
 
-public class MaterialShopItem : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler
+public class MaterialShopItem : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
     [SerializeField] private Image m_image;
     [SerializeField] private Button m_button;
@@ -14,6 +14,7 @@ public class MaterialShopItem : MonoBehaviour, ISelectHandler, IDeselectHandler,
     private Item m_itemData;
     private MaterialsInventory m_materialsInventory;
     private PlayerStats playerStats;
+    private Color m_emptyColor = new Color(1, 1, 1, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -27,9 +28,18 @@ public class MaterialShopItem : MonoBehaviour, ISelectHandler, IDeselectHandler,
         m_itemData = _item;
         m_image.sprite = _item.Icon;
         m_image.color = Color.white;
+        m_button.interactable = true;
+
         //m_itemName.text = _item.ItemName;
         m_itemPrice.text = _item.price.ToString();
         m_materialsInventory = materialsInventory;
+    }
+
+    public void Clear()
+    {
+        m_image.color = m_emptyColor;
+        m_itemPrice.text = string.Empty;
+        m_button.interactable = false;
     }
 
     private void PurchaseMaterial()
@@ -40,6 +50,11 @@ public class MaterialShopItem : MonoBehaviour, ISelectHandler, IDeselectHandler,
         m_materialsInventory.AddItem(m_itemData);
         GameManager.instance.AddItemEvent.Invoke(m_itemData);
         GameManager.instance.BuyEvent.Invoke(-m_itemData.price);
+    }
+
+    public void SelectIten()
+    {
+        m_button.Select();
     }
 
     public void OnSelect(BaseEventData eventData)
@@ -55,7 +70,7 @@ public class MaterialShopItem : MonoBehaviour, ISelectHandler, IDeselectHandler,
         //m_ouline.DOFade(0, 0.5f).SetEase(Ease.OutCubic);
         m_image.rectTransform.DOScale(1f, 0.3f).SetEase(Ease.OutExpo);
     }
-
+    /*
     public void OnPointerEnter(PointerEventData eventData)
     {
         m_image.rectTransform.DOScale(1.1f, 0.3f).SetEase(Ease.InExpo);
@@ -64,5 +79,5 @@ public class MaterialShopItem : MonoBehaviour, ISelectHandler, IDeselectHandler,
     public void OnPointerExit(PointerEventData eventData)
     {
         m_image.rectTransform.DOScale(1f, 0.3f).SetEase(Ease.OutExpo);
-    }
+    }*/
 }
